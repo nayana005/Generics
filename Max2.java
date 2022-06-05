a8eee76479f74a729ae3ac48a805139bf0d42e00
nayana005 <nayanasuresh1005@gmail.com>
2022-06-05 20:22:59 +0530

    Added MaxFloat amoung 3

---
+++ Max2.java
@@ -0,0 +1,13 @@
+package com.bl.generics;
+
+public class Max2 {
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Program For Finding Maximum Number Among Three");
+		MaximumFloat compare = new MaximumFloat();
+		compare.maximumInteger();
+		compare.maximumFloat();
+
+
+	}
+
+}
