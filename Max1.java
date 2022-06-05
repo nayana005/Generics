a71855398106ac679d64bcd63c757a820d06c8ce
nayana005 <nayanasuresh1005@gmail.com>
2022-06-05 20:18:01 +0530

    Added MaxInterger Amoung 3

---
+++ Max1.java
@@ -0,0 +1,15 @@
+package com.bl.generics;
+
+public class Max1 {
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Program For Finding Maximum Number Among Three");
+		MaximumInteger compare = new MaximumInteger();
+		compare.max_int();
+
+	}
+
+}
+
+    
+
+
