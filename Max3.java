70184628faaf352396f8882ec95328ad691a3ad8
nayana005 <nayanasuresh1005@gmail.com>
2022-06-05 20:29:22 +0530

    Added MaxString amoung 3

---
+++ Max3.java
@@ -0,0 +1,13 @@
+package com.bl.generics;
+
+public class Max3 {
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Program For Finding Maximum Number Among Three");
+		MaximumString compare = new MaximumString();
+		compare.maximumInteger();
+		compare.maximumFloat();
+		compare.maximumString();
+	}
+
+
+}
